79a368b470db7986253873e7c07277af81262763
Haris-eng7 <89679109+Haris-eng7@users.noreply.github.com>
2024-10-21 16:51:16 +0500

    Design catalog pages fixed

--- src/app/design-catalog/ui/ProductList.jsx
+++ src/app/design-catalog/ui/ProductList.jsx
@@ -4,8 +4,8 @@ import ProductCard from "@/components/cards/ProductCard";
 
 const ProductList = ({ products }) => {
   return (
-    <div className="px-16 py-12">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="px-6 lg:px-16 py-12">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -8,7 +8,9 @@ const Footer = ({ textColor = "light-green" }) => {
     <footer className="px-0 lg:px-4 py-4 bg-transparent">
       <div className="flex justify-between flex-col lg:flex-row text-center lg:text-left">
         <span className={`text-${textColor} font-prestiege text-sm`}>
-          Copyright © 2023 Open Design Solutions - All Rights Reserved.
+          Copyright © 2023 Open Design Solutions{" "}
+          <span className="hidden lg:contents">-</span>{" "}
+          <br className="lg:hidden" /> All Rights Reserved.
         </span>
 
         <Link
