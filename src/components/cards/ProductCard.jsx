c4c8e144e5b6922f543fc1f5c1039412e9c6ee1b
Haris-eng7 <89679109+Haris-eng7@users.noreply.github.com>
2024-10-25 12:34:28 +0500

    Product card fix

--- src/components/cards/ProductCard.jsx
+++ src/components/cards/ProductCard.jsx
@@ -3,11 +3,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import Image from "next/image";
 import Link from "next/link";
-
+import CartIcon from "@/assets/cart.svg";
+import BagIcon from "@/assets/bag.svg";
+import EyeIcon from "@/assets/eye.svg";
 const ProductCard = ({ product }) => {
   return (
-    <div key={product.id} className="flex flex-col items-center text-center">
-      <Link href={product?.link || "/"}>
+    <div
+      key={product.id}
+      className="flex group/item flex-col items-center text-center"
+    >
+      <Link className=" hover:bg-slate-100" href={product?.link || "/"}>
         <div className="bg-gray-200 rounded-md mb-2">
           <Image
             src={product.image}
@@ -22,6 +27,32 @@ const ProductCard = ({ product }) => {
           {product.price}
         </p>
       </Link>
+      <div className="flex flex-row gap-1 absolute mt-2">
+        <a
+          className="group/edit p-1 rounded-full  bg-white-green invisible hover:bg-slate-200 group-hover/item:visible"
+          href="#"
+        >
+          <span>
+            <CartIcon />
+          </span>
+        </a>
+        <a
+          className="group/edit p-1 rounded-full  bg-white-green invisible hover:bg-slate-200 group-hover/item:visible"
+          href="#"
+        >
+          <span>
+            <BagIcon />
+          </span>
+        </a>
+        <a
+          className="group/edit  p-1 rounded-full bg-white-green invisible hover:bg-slate-200 group-hover/item:visible"
+          href="#"
+        >
+          <span>
+            <EyeIcon />
+          </span>
+        </a>
+      </div>
     </div>
   );
 };
